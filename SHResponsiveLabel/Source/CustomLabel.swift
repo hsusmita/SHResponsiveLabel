e03fa8ea4e14f2997b308d60180ca0ebac07a28a
Susmita Horrow <susmitahorrow@Susmitas-MacBook-Pro.local>
2015-08-24 10:34:31 +0530

    WIP

--- SHResponsiveLabel/Source/CustomLabel.swift
+++ SHResponsiveLabel/Source/CustomLabel.swift
@@ -2,51 +2,45 @@
 //  CustomizedLabel.swift
 //  SHResponsiveLabel
 //
-//  Created by sah-fueled on 21/08/15.
+//  Created by hsusmita on 21/08/15.
 //  Copyright (c) 2015 hsusmita.com. All rights reserved.
 //
 
 import UIKit
 
-let gesturedsddfsdfsfd: UITouchGestureRecognizer = UITouchGestureRecognizer()
 
 public class CustomLabel: UILabel {
   
-  //	let textkitStack:TextkitStack
+  	let textkitStack:TextkitStack
+	
+  	override init(frame: CGRect) {
+  		textkitStack = TextkitStack()
   
-  //	override init(frame: CGRect) {
-  //		textkitStack = TextkitStack()
-  //    gesture = UITouchGestureRecognizer()
-  //
-  //		super.init(frame: frame)
-  //
-  ////		setup()
-  //		configureGestures()
-  //	}
-  //	override public func layoutSubviews() {
-  //		textkitStack.textContainer.size = self.bounds.size;
-  //	}
-  //	required public init(coder aDecoder: NSCoder) {
-  //
-  //		textkitStack = TextkitStack()
-  //
-  ////		super.init(coder: aDecoder)
-  ////		textkitStack.textContainer.lineBreakMode = self.lineBreakMode;
-  ////    userInteractionEnabled = true
-  ////    gesture = UITouchGestureRecognizer()
-  ////    addGestureRecognizer(gesture)
-  ////    textkitStack.gestureRecognizer = gesture
-  ////    configureGestures()
-  //	}
+  		super.init(frame: frame)
   
-  func printc() {
-    
-  }
-  public  override func awakeFromNib() {
+  //		setup()
+  		configureGestures()
+  	}
+  	override public func layoutSubviews() {
+  		textkitStack.textContainer.size = self.bounds.size;
+  	}
+  	required public init(coder aDecoder: NSCoder) {
+  
+  		textkitStack = TextkitStack()
+  		super.init(coder: aDecoder)
+  		textkitStack.textContainer.lineBreakMode = self.lineBreakMode;
+      userInteractionEnabled = true
+//      let gesture = UITouchGestureRecognizer()
+//      addGestureRecognizer(gesture)
+//      textkitStack.gestureRecognizer = gesture
+  //    configureGestures()
+  	}
+	
+   public  override func awakeFromNib() {
     initialTextConfiguration()
-    //    textkitStack.textContainer.lineBreakMode = self.lineBreakMode;
-    
-    //		layoutIfNeeded()
+        textkitStack.textContainer.lineBreakMode = self.lineBreakMode;
+	
+    		layoutIfNeeded()
     super.awakeFromNib()
     
   }
@@ -65,7 +59,7 @@ public class CustomLabel: UILabel {
   override public var text: String! {
     didSet {
       let currentText = NSAttributedString(string: text!, attributes: attributesFromProperties())
-      //			textkitStack.updateTextStorage(currentText)
+      			textkitStack.updateTextStorage(currentText)
     }
   }
   func configureGestures() {
@@ -106,14 +100,14 @@ public class CustomLabel: UILabel {
   
   
   public override func drawTextInRect(rect: CGRect) {
-    //		textkitStack.drawTextInRect(rect)
+    		textkitStack.drawTextInRect(rect)
   }
-  //
-  //	public override func textRectForBounds(bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
-  ////    let requiredRect = textkitStack.rectFittingText(bounds.size, lineCount: numberOfLines, font: self.font)
-  ////		return requiredRect
-  //	}
   
+  	public override func textRectForBounds(bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+      let requiredRect = textkitStack.rectFittingText(bounds.size, lineCount: numberOfLines, font: self.font)
+  		return requiredRect
+  	}
+	
   public override class func requiresConstraintBasedLayout()-> Bool {
     return true
   }
@@ -139,48 +133,48 @@ public class CustomLabel: UILabel {
 //	}
 //}
 
-//extension CustomLabel {
-//
-//  public override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-//    let gesture = UITapGestureRecognizer()
-//
-//    let touchLocation = (touches.first as! UITouch).locationInView(self)
-//    let index = textkitStack.characterIndexAtLocation(touchLocation)
-//    println("index = \(index)")
+extension CustomLabel {
+
+  public override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    let gesture = UITapGestureRecognizer()
+
+    let touchLocation = (touches.first as! UITouch).locationInView(self)
+    let index = textkitStack.characterIndexAtLocation(touchLocation)
+    println("index = \(index)")
 //    let rangeOfTappedText = textkitStack.rangeLocation(touchLocation)
 //     let shouldDetectTouch = textkitStack.shouldHandleTouchAtIndex(index) && !patternTouchInProgress()
-////    var rangeOfTappedText = NSMakeRange(NSNotFound, 0)
-////    if let currentString = textStorage as NSAttributedString? {
-////      if index < currentString.length {
-////        rangeOfTappedText = layoutManager.rangeOfNominallySpacedGlyphsContainingIndex(index)
-////      }
-////    }
-////    let shouldDetectTouch = shouldHandleTouchAtIndex(index) && !patternTouchInProgress()
-////    if shouldDetectTouch {
-////      handleTouchBeginForRange(rangeOfTappedText)
-////    }else {
-////      super.touchesBegan(touches, withEvent: event)
-////    }
-//  }
-//
-//  public override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-//    super.touchesMoved(touches, withEvent: event)
-//  }
-//
-//  public override func touchesCancelled(touches: Set<NSObject>!, withEvent event: UIEvent!) {
-//    super.touchesCancelled(touches, withEvent: event)
-//  }
-//
-//  public override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
-////    if patternTouchInProgress() && shouldHandleTouchAtIndex(selectedRange!.location) {
-////      removeHighlightingForIndex(selectedRange!.location)
-////      dispatch_after(dispatch_time(DISPATCH_TIME_NOW,Int64(0.05 * Double(NSEC_PER_SEC))),
-////        dispatch_get_main_queue(),{
-////          self.handleTouchEnd()
-////      })
-////    }else {
-////      super.touchesEnded(touches, withEvent: event)
-////    }
-//  }
-//}
-//
+//    var rangeOfTappedText = NSMakeRange(NSNotFound, 0)
+//    if let currentString = textStorage as NSAttributedString? {
+//      if index < currentString.length {
+//        rangeOfTappedText = layoutManager.rangeOfNominallySpacedGlyphsContainingIndex(index)
+//      }
+//    }
+//    let shouldDetectTouch = shouldHandleTouchAtIndex(index) && !patternTouchInProgress()
+//    if shouldDetectTouch {
+//      handleTouchBeginForRange(rangeOfTappedText)
+//    }else {
+//      super.touchesBegan(touches, withEvent: event)
+//    }
+  }
+
+  public override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+    super.touchesMoved(touches, withEvent: event)
+  }
+
+  public override func touchesCancelled(touches: Set<NSObject>!, withEvent event: UIEvent!) {
+    super.touchesCancelled(touches, withEvent: event)
+  }
+
+  public override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+//    if patternTouchInProgress() && shouldHandleTouchAtIndex(selectedRange!.location) {
+//      removeHighlightingForIndex(selectedRange!.location)
+//      dispatch_after(dispatch_time(DISPATCH_TIME_NOW,Int64(0.05 * Double(NSEC_PER_SEC))),
+//        dispatch_get_main_queue(),{
+//          self.handleTouchEnd()
+//      })
+//    }else {
+//      super.touchesEnded(touches, withEvent: event)
+//    }
+  }
+}
+
